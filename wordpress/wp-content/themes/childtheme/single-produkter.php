48a3c47575c1820f5627e0ceb742ab3bb181f05c
Pernille Strate <pernillestrate@Pernilles-MacBook-Pro.local>
2021-06-03 12:32:11 +0200

    flere produkter på single view

--- wordpress/wp-content/themes/childtheme/single-produkter.php
+++ wordpress/wp-content/themes/childtheme/single-produkter.php
@@ -23,53 +23,49 @@ get_header(); ?>
         </section>
     </article>
 
-    <div class="baggrund">
-        <h2 class="flere_produkter">Du vil måske også synes om:</h2>
-    </div>
+    <section class="baggrund">
+        <h2 class="h2_single">Du vil måske også synes om:</h2>
+        <div class="flere_produkter">
+            <a href="http://pernillestrate.dk/floweratelier/wordpress/produkter/louis-vuitton-catwalk-coffee-table-book/">
+                <div class="img-hover-zoom2">
+                    <img class="flere" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/bog_louis.jpg" alt="louis-bog">
+                </div>
+            </a>
+            <a href="http://pernillestrate.dk/floweratelier/wordpress/produkter/lyserode-bloklys/">
+                <div class="img-hover-zoom2">
+                    <img class="flere" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/lys_lyserodpink.jpg" alt="lys">
+                </div>
+            </a>
+            <a href="http://pernillestrate.dk/floweratelier/wordpress/produkter/velduftende-meraki-handsaebe/">
+                <div class="img-hover-zoom2">
+                    <img class="flere" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/meraki.jpg" alt="meraki">
+                </div>
+            </a>
+            <a href="http://pernillestrate.dk/floweratelier/wordpress/produkter/rustik-glas-vase/">
+                <div class="img-hover-zoom2">
+                    <img class="flere" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/vase_sjov.jpg" alt="vase">
+                </div>
+            </a>
+        </div>
+        <button class="knap_single">Shop mere til boligen</button>
+    </section>
 
-    <!--
-    <div class="baggrund">
-        <h2 class="flere_produkter">Du vil måske også synes om:</h2>
-        <section id="flereprodukter">
-            <template>
-                <article class="article_flereprodukter">
-                    <img src="" alt="" class="billede_flereprodukter">
-                    <div>
-                        <h3 class="flereprodukter_title"></h3>
-                        <p class="beskrivelse"></p>
-                        <a href="">Læs mere</a>
-                    </div>
-                </article>
-            </template>
-        </section>
-    </div>
--->
+
 </main>
 
 
 <script>
     let produkter;
-    //    let flereprodukter;
     let aktuelprodukt = <?php echo get_the_ID() ?>;
 
     const dbUrl = "http://pernillestrate.dk/floweratelier/wordpress/wp-json/wp/v2/produkter/" + aktuelprodukt;
 
-    //    const flereprodukterUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder?per_page=100";
-
-    //    const container = document.querySelector("#flereprodukter");
-
-    //    console.log("container: ", container);
     async function getJson() {
         const data = await fetch(dbUrl);
         produkter = await data.json();
         console.log(produkter);
 
-        //        const data2 = await fetch(flereprodukterUrl);
-        //        flereprodukter = await data2.json();
-        //        console.log("flereprodukter: ", flereprodukter);
-
         visProdukter();
-        //        visFlereprodukter();
     }
 
 
@@ -82,32 +78,7 @@ get_header(); ?>
         document.querySelector(".title_single").textContent = produkter.title.rendered;
     }
 
-    //    function visFlereprodukter() {
-    //        console.log("visFlereprodukter");
-    //        let temp = document.querySelector("template");
-    //        flereprodukter.forEach(flereprodukter => {
-    //                console.log("loop id :", aktuelprodukt);
-    //                if (flereprodukter.horer_til_produkt == aktuelprodukt {
-    //
-    //                        console.log("loop kører id :", aktuelprodukt);
-    //                        let klon = temp.cloneNode(true).content;
-    //                        klon.querySelector("h3").textContent = flereprodukter.title.rendered;
-    //                        klon.querySelector("img").src = flereprodukter.billede.guid;
-    //                        console.log("flereprodukter.title.rendered: ", flereprodukter.title.rendered);
-    //                        klon.querySelector(".beskrivelse").innerHTML = flereprodukter.content.rendered;
-    //                        klon.querySelector("article").addEventListener("click", () => {
-    //                            location.href = flereprodukter.link;
-    //                        })
-    //
-    //                        klon.querySelector("a").href = flereprodukter.link;
-    //                        console.log("episode", flereprodukter.link);
-    //                        container.appendChild(klon);
-    //                    }
-    //
-    //
-    //                })
-    //
-    //        }
+
     getJson();
 
 </script>
